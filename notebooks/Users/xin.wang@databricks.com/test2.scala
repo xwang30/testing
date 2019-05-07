8aea6a82ab9cecc35621d0cb9fb2d8f40216ccc0
xwang30 <50154228+xwang30@users.noreply.github.com>
2019-05-07 10:21:40 -0700

    my first commit

---
+++ notebooks/Users/xin.wang@databricks.com/test2.scala
@@ -0,0 +1,24 @@
+// Databricks notebook source
+dbutils.fs.ls("dbfs:/databricks/init/")
+
+// COMMAND ----------
+
+dbutils.fs.put("/databricks/init/global.sh","""
+echo $DB_IS_DRIVER
+if [[ $DB_IS_DRIVER = "TRUE" ]]; then
+  echo driver >> test
+else
+  echo worker >> test
+fi
+echo “we work together” >>test""")
+
+// COMMAND ----------
+
+dbutils.fs.rm("/databricks/init/global.sh")
+
+// COMMAND ----------
+
+// MAGIC %sh cat /test
+
+// COMMAND ----------
+
