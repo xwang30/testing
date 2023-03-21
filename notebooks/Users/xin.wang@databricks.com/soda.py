64ee6640420b90937d8e8b6f2cf4cc0522f041d7
xwang30 <xwang30>
2023-03-21 01:03:12 +0000

    add repo

---
+++ notebooks/Users/xin.wang@databricks.com/soda.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %pip freeze
+# MAGIC 
+# MAGIC !python --version
+# MAGIC 
+# MAGIC import soda.scan
+# MAGIC 
+# MAGIC from soda import scan
+# MAGIC 
+# MAGIC from soda.scan import Scan
+# MAGIC 
+# MAGIC from common_utils.data_quality.soda_scan import Scan
